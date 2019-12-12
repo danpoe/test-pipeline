91622fd15565c4dec483f5eedbc0b4f1bcfafd53
Daniel Poetzl <dan.poetzl@gmail.com>
2019-12-12 10:23:13 +0000

    More checks for RunPass test

--- test/test_run_pass.py
+++ test/test_run_pass.py
@@ -5,9 +5,22 @@ import unittest
 class Test(unittest.TestCase):
   def test_run_pass_basic(self):
     p = rt.RunPass(['echo', 'test'], ignore_file=True)
-    with tempfile.TemporaryFile('w') as f1,\
-      tempfile.TemporaryFile('w') as f2,\
-      tempfile.TemporaryFile('w') as f3:
-      p('ignored', [f1, f2, f3])
 
-    
+    with tempfile.TemporaryFile('w+') as f_stdout,\
+      tempfile.TemporaryFile('w+') as f_stderr,\
+      tempfile.TemporaryFile('w+') as f_data:
+
+      p('ignored', [f_stdout, f_stderr, f_data])
+
+      f_stdout.seek(0)
+      s = f_stdout.read()
+      self.assertEqual(s, 'test\n')
+
+      f_stderr.seek(0)
+      s = f_stderr.read()
+      self.assertEqual(s, '')
+
+      l = ['Timeout', 'User time', 'Sys time', 'Real time', 'Exit code']
+      f_data.seek(0)
+      for i, line in enumerate(f_data):
+        self.assertTrue(line.startswith(l[i]))
