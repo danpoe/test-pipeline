c77a3b4784c5cb2efda901ea495365c85b2fe511
Daniel Poetzl <dan.poetzl@gmail.com>
2020-01-10 20:01:54 +0000

    Add test for _copy_and_merge()

--- test/test_run_tests.py
+++ test/test_run_tests.py
@@ -1,5 +1,7 @@
+import pathlib
 import run_tests as rt
 import unittest
+import tempfile
 
 class Test(unittest.TestCase):
   def test_validate_urls(self):
@@ -25,4 +27,36 @@ class Test(unittest.TestCase):
     self.assertTrue(rt._is_valid_entry('archive{rt._archive_suf'))
 
   def test_copy_and_merge(self):
-    pass
+    with tempfile.TemporaryDirectory() as d:
+      # we use suffixes to indicate whether files and folders are unique (`u`)
+      # or common (`c`) between the two top-level folders that are merged
+      d = pathlib.Path(d)
+      # source folder
+      d1 = d / 'dir1u'
+      d1.mkdir()
+      (d1 / 'file1u').touch()
+      (d1 / 'file2c').touch()
+      (d1 / 'dir2u').mkdir()
+      (d1 / 'dir2u' / 'file3u').touch()
+      (d1 / 'dir3c').mkdir()
+      (d1 / 'dir3c' / 'file3u').touch()
+      (d1 / 'dir3c' / 'file4c').touch()
+      # target folder
+      d2 = d / 'dir2u'
+      d2.mkdir()
+      (d2 / 'file2c').touch()
+      (d2 / 'dir3c').mkdir()
+      (d2 / 'dir3c' / 'file4c').touch()
+      (d2 / 'dir3c' / 'file5u').touch()
+      # now copy and merge
+      rt._copy_and_merge(d1, d2)
+      # check result
+      self.assertTrue(d2.is_dir())
+      self.assertTrue((d2 / 'file1u').is_file())
+      self.assertTrue((d2 / 'file2c').is_file())
+      self.assertTrue((d2 / 'dir2u').is_dir())
+      self.assertTrue((d2 / 'dir2u' / 'file3u').is_file())
+      self.assertTrue((d2 / 'dir3c').is_dir())
+      self.assertTrue((d2 / 'dir3c' / 'file3u').is_file())
+      self.assertTrue((d2 / 'dir3c' / 'file4c').is_file())
+      self.assertTrue((d2 / 'dir3c' / 'file5u').is_file())
