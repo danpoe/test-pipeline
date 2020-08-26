84069328645f2b32002d5ebd5ee945af7b6508bf
Daniel Poetzl <dan.poetzl@gmail.com>
2020-08-26 20:54:38 +0100

    Adjust driver example for new test pipeline

--- drivers/example/example.py
+++ drivers/example/example.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
 
 '''
-Template that illustrates how to benchmark a tool using the run tests framework
+Template that illustrates how to benchmark a tool using the test pipeline
 
 Usage:
 ./example [--input-file <file>]
 '''
 
-import run_tests as rt
+import test_pipeline as tp
 
 def _main():
-  rt.setup()
+  tp.setup()
 
   # Check property 1
   cmd = ['echo', '--arg']
-  p = rt.CheckPass(cmd, retcode=lambda r: r == 0)
-  rt.add_pass(p)
+  p = tp.CheckPass(cmd, retcode=lambda r: r == 0)
+  tp.add_pass(p)
 
   # Check property 2
   cmd = ['echo' , '--arg']
-  p = rt.CheckPass(cmd, regex_stdout=r'example')
-  rt.add_pass(p)
+  p = tp.CheckPass(cmd, regex_stdout=r'example')
+  tp.add_pass(p)
 
   # Run analysis
   cmd = ['echo', '--arg1', '--arg2']
-  p = rt.RunPass(cmd, timeout=1800)
-  rt.add_pass(p)
+  p = tp.RunPass(cmd, timeout=1800)
+  tp.add_pass(p)
 
-  rt.test()
+  tp.test()
 
 
 if __name__ == '__main__':
